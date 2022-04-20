90432750cbd4eb54de10ab7ac4e0b7637808c9f3
MGH <konstantin.a.smolkov@gmail.com>
2022-04-20 15:17:48 +0300

    доделал отрицательные числа

--- app/src/main/java/com/example/random_number/MainActivity.kt
+++ app/src/main/java/com/example/random_number/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.random_number
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -16,40 +14,34 @@ class MainActivity : AppCompatActivity() { // тут идёт наследова
     private fun randomNumber(left: Int, right: Int): Int {
         var someNumber = abs(java.util.Calendar.getInstance().timeInMillis.toInt())
 
-        val first = someNumber shl (someNumber % 34 + 1)
-        val second = someNumber shr (someNumber % 17 + 1)
-        val third = someNumber shl (someNumber % 30 + 1)
-
+        val first = someNumber shl 13
+        val second = someNumber shr 10
+        val third = someNumber shl 13
         someNumber = abs(someNumber or first or second or third)
 
-        var generalNumber: Int
-        val rightSomeNumber = right - someNumber % right
-        val leftSomeNumber = left - someNumber % left
-        if (rightSomeNumber > left) {
-            generalNumber = rightSomeNumber
-        } else {
-            val diffRightandLeft = abs(rightSomeNumber - leftSomeNumber)
-            generalNumber = left + diffRightandLeft
-            if (generalNumber > right) {
-                generalNumber = right
-            }
-        }
+        val diffRightandLeft = (right - left) + 1
+        val generalNumber = someNumber % diffRightandLeft + left
+
         return generalNumber
     }
+
+    var res: TextView? = null
+    var button: Button? = null
+    var from: EditText? = null
+    var to: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        val res: TextView = findViewById(R.id.result)
-        val button: Button = findViewById(R.id.button)
+        res = findViewById(R.id.result)
+        button = findViewById(R.id.button)
 
-        button.setOnClickListener {
+        button?.setOnClickListener {
 
-            val from: EditText? = findViewById(R.id.from)
-            val to: EditText? = findViewById(R.id.to)
-
+            from = findViewById(R.id.from)
+            to = findViewById(R.id.to)
 
             val left = from?.text.toString().trim().toIntOrNull()
             val right = to?.text.toString().trim().toIntOrNull()
@@ -61,7 +53,7 @@ class MainActivity : AppCompatActivity() { // тут идёт наследова
                     Toast.makeText(this, "Границы введены неверно", Toast.LENGTH_SHORT).show()
                 } else {
 
-                    res.text = randomNumber(left, right).toString()
+                    res?.text = randomNumber(left, right).toString()
                 }
             }
         }
